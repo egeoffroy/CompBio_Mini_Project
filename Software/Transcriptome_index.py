eab272b08d7ce6d6613bf6c5446469bc597b3cad
Elyse Geoffroy <egeoffroy@luc.edu>
2020-02-16 12:18:18 -0600

    Update Transcriptome_index.py

--- Software/Transcriptome_index.py
+++ Software/Transcriptome_index.py
@@ -1,16 +1,4 @@
-#Create Transcriptome Index for HCMV
-from Bio import SeqIO
-from Bio import Entrez
-Entrez.email = "ecgeoffroy@gmail.com"
 
-handle = Entrez.efetch(db="nucleotide", id=["EF999921"], rettype="fasta") #obtain plain text records of the GenBank ids in FASTA format from NCBI's [Nucleotide] database.
-records = list(SeqIO.parse(handle, "fasta"))
-SeqIO.write(records[0],'EF999921.fasta', 'fasta') #write out the fasta file for bowtie2
-
-outfile = open("CDS_EF999921.txt", 'w') #open write out file
-i = 0 #use i to count the number of CDS entries written out to the file
-for rec in SeqIO.parse('EF999921.gb', 'genbank'): #open the genBank file
-        if rec.features: #look at the genbank features
 #Create Transcriptome Index for HCMV
 from Bio import SeqIO
 from Bio import Entrez
