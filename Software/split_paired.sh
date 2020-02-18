e89939a7876150c764ed2f770d1789fcb247c2a7
Elyse Geoffroy <egeoffroy@luc.edu>
2020-02-17 18:36:03 -0600

    Update split_paired.sh

--- Software/split_paired.sh
+++ Software/split_paired.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#an extra script in case the user only has the SRA files and needs to separate them
+#This is an extra step in case the user only has the SRA files and needs to separate them into paired reads
 declare -a SRRs
 SRRs=(SRR5660030 SRR5660033 SRR5660044 SRR5660045)
 for i in "${SRRs[@]}":
