be43c6ca0cb2c44da7cac82317a825a5d5ef1dee
Elyse Geoffroy <egeoffroy@luc.edu>
2020-02-17 18:31:49 -0600

    Update contigs_count.py

--- Software/contigs_count.py
+++ Software/contigs_count.py
@@ -1,17 +1,20 @@
 #Return number of contigs longer than 1000 bp
 #Returns files of significant length contigs (>1000)
+
+#Import libraries
 from Bio.Seq import Seq
 from Bio import SeqIO
-sequences = []
+
+sequences = [] #make empty array
 for record in SeqIO.parse("contigs.fasta", "fasta"):
-    sequences.append(str(record.seq))
+    sequences.append(str(record.seq)) #add fasta sequences to the array sequences
 
 count = 0
-outfile = open("./significant_contigs.txt", "w")
-for contig in sequences:
-    if len(contig) > 1000:
-        count +=1
+outfile = open("./significant_contigs.txt", "w") # open new file
+for contig in sequences: #for each of the sequences (contigs)
+    if len(contig) > 1000: #check if the length of bp is > 1000
+        count +=1 #if it is, add one to count and write out the contig to the file
         outfile.write(contig + '\n')
-print(count)
+print(count) #print out the count of contigs > 1000bp
 outfile.close()
 
