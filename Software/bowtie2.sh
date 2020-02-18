42e0e3ebaa6e0aaab977b5d6333c55b4b0f3a029
Elyse Geoffroy <egeoffroy@luc.edu>
2020-02-17 18:53:43 -0600

    Update bowtie2.sh

--- Software/bowtie2.sh
+++ Software/bowtie2.sh
@@ -6,5 +6,5 @@ bowtie2-build ./EF999921.fasta EF999921 #run build
 #for each of the SRRs, run Bowtie --> using single reads instead of paired reads
 for i in "${SRR[@]}" 
 do
-        bowtie2 --quiet -x EF999921 -1 ./${i}_1.fastq.gz -2 ./${i}_2.fastq.gz -S EF999921_${i}.sam #output is a .sam file
+        bowtie2 --quiet -x EF999921 -1 ./${i}_1.fastq -2 ./${i}_2.fastq -S EF999921_${i}.sam #output is a .sam file
 done
