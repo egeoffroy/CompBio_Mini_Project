50ad423519c0714521467908dd494b1dc4d5967c
Elyse Geoffroy <egeoffroy@luc.edu>
2020-02-17 18:50:56 -0600

    Update kallisto.sh

--- Software/kallisto.sh
+++ Software/kallisto.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #Create the transcriptome reference index
-time kallisto index -i HCMV_index.idx CDS_EF999921.txt
+time kallisto index -i HCMV_index.idx EF999921.fasta
 
 # For each of the user inputted SRRs, run kallisto quantification with their read paired separated files
 declare -a SRR
 SRR=($1 $2 $3 $4)
 for i in "${SRR[@]}";
 do
-        time kallisto quant -i index.idx -o ./"${i}" -b 30 -t 4 ./"${i}"_1.fastq.gz ./"${i}"_2.fastq.gz
+        time kallisto quant -i HCMV_index.idx -o ./"${i}" -b 30 -t 4 ./"${i}"_1.fastq.gz ./"${i}"_2.fastq.gz
 done
