c4b8084060a4184e3e0e1a483a9e9995fd5f814f
Elyse Geoffroy <egeoffroy@luc.edu>
2020-02-18 16:01:20 -0600

    Update split_paired.sh

--- Software/split_paired.sh
+++ Software/split_paired.sh
@@ -2,7 +2,7 @@
 
 #This is an extra step in case the user only has the SRA files and needs to separate them into paired reads
 declare -a SRRs
-SRRs=(SRR5660030 SRR5660033 SRR5660044 SRR5660045)
+SRRs=($1 $2 $3 $4)
 for i in "${SRRs[@]}":
 do
         fastq-dump --split-files -O ./ "${i}"
