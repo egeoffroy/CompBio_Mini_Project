51593241a1254b9c0c384648c25b7404330a7c9e
Elyse Geoffroy <egeoffroy@luc.edu>
2020-02-25 11:25:58 -0600

    Update sleuth.R

--- sleuth.R
+++ sleuth.R
@@ -24,11 +24,9 @@ sleuth_table <- sleuth_results(so, 'reduced:full', 'lrt', show_all = FALSE)
 #filter most significant results (FDR/qval < 0.05)
 sleuth_significant<- dplyr::filter(sleuth_table, qval <= 0.05) %>% dplyr::arrange(pval)
 head(sleuth_significant)
-#sleuth_significnat <- dplyr::select(sleuth_significant, target_id, pval, qval)
-#write top 10 transcripts to file
+#write top transcripts to file
 sleuth_significant <- data.frame(sleuth_significant$target_id, sleuth_significant$test_stat, sleuth_significant$pval, sleuth_significant$qval)
 colnames(sleuth_significant) <- c("target_id", "test_stat", "pval","qval")
 sleuth_significant <- na.omit(sleuth_significant)
-#write.table(sleuth_significant, file="sleuth_all_hits.txt", quote=F, row.names=F)
-#sleuth_significant_top <- sleuth_significant
+
 write.table(sleuth_significant, file="sleuth_output.txt",quote = FALSE,row.names = FALSE)
