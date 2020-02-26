1f511a4fad754080aa606b64b81014c849599ea8
Elyse Geoffroy <egeoffroy@luc.edu>
2020-02-26 09:42:06 -0600

    Delete sam_to_fastq.sh

--- sam_to_fastq.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#This file takes the sam output files from Bowtie2 and converts them into fastq files for Spades
-sam=$1
-cat EF999921_${1}.sam | grep -v ^@ | awk '{print "@"$1"\n"$10"\n+\n"$11}' > EF999921_${1}.fastq
-
-
